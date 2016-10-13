28914710b35da01d0f46c48f6717f8c7995e8aba
Ben Bales <bbbales2@gmail.com>
2016-10-13 08:54:35 -0700

    Updates

--- check_rotations_cubic_orthorhombic.py
+++ check_rotations_cubic_orthorhombic.py
@@ -13,8 +13,13 @@ import itertools
 q1 = quaternion.Quaternion([0.98849515554696088, -0.0026722254086214156, 0.015326741589590076, 0.15045024979640861])
 q2 = quaternion.Quaternion([0.98849515554696088, -0.0026722254086214156, -0.015326741589590076, 0.15045024979640861])
 
+# Get angle between two quaternions
 deltaQ = q1 * q2.conjugate()
+
+# Find all possible representations of this rotation in mix of Cubic + Orthorhombic symmetries
 equivMiso = [(q * j.conjugate()).supplement() for q, j in itertools.product([i * deltaQ for i in symmetry.Symmetry.Cubic.quOperators()], symmetry.Symmetry.Orthorhombic.quOperators())] # compute all equivilent misorientation representations
+
+# Pick the smallest angle one
 d = max(max(equivMiso), max([q.conjugate() for q in equivMiso])) # select quaternion of smallest rotation with axis in most positive quadrant from equivilent rotations w/ switching symmetry
 
 print "Quaternion rotation between q1 & q2", d
