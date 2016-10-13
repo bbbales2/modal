fd1ebf713b673c3730c86fcd714eee030222d3c9
Ben Bales <bbbales2@gmail.com>
2016-10-13 08:34:45 -0700

    Updates

--- check_rotations_cubic_orthorhombic.py
+++ check_rotations_cubic_orthorhombic.py
@@ -6,31 +6,17 @@ os.chdir('/home/bbales2/modal')
 
 from rotations import symmetry
 from rotations import quaternion
+
 import itertools
 # get the equivalent passive rotation in the fundamental zone (active post multiplies symmetry operator)
 
-def fzQuat(qu):
-    output = []
-    for sym1 in symmetry.Symmetry.Cubic.quOperators():
-        for sym2 in symmetry.Symmetry.Orthorhombic.quOperators():
-            output.append((sym1 * qu * sym2.conjugate()).supplement().wxyz)
+q1 = quaternion.Quaternion([0.98849515554696088, -0.0026722254086214156, 0.015326741589590076, 0.15045024979640861])
+q2 = quaternion.Quaternion([0.98849515554696088, -0.0026722254086214156, -0.015326741589590076, 0.15045024979640861])
 
-    return numpy.array(output)
+deltaQ = q1 * q2.conjugate()
+equivMiso = [(q * j.conjugate()).supplement() for q, j in itertools.product([i * deltaQ for i in symmetry.Symmetry.Cubic.quOperators()], symmetry.Symmetry.Orthorhombic.quOperators())] # compute all equivilent misorientation representations
+d = max(max(equivMiso), max([q.conjugate() for q in equivMiso])) # select quaternion of smallest rotation with axis in most positive quadrant from equivilent rotations w/ switching symmetry
 
-    #return [(q * j.conjugate()).supplement() for q, j in itertools.product([i * qu for i in symmetry.Symmetry.Orthorhombic.quOperators()], symmetry.Symmetry.Orthorhombic.quOperators())]
+print "Quaternion rotation between q1 & q2", d
+print "Difference in angle: {0} degrees".format(2 * numpy.arccos(d.wxyz[0]) * 180 / numpy.pi)
 
-w, x, y, z = (0.98849515554696088, -0.0026722254086214156, 0.015326741589590076, 0.15045024979640861)
-
-o1 = fzQuat(quaternion.Quaternion([w, x, y, z]))
-o2 = fzQuat(quaternion.Quaternion([w, x, -y, z]))
-
-distances = []
-idxs = []
-for i in range(len(o1)):
-    for j in range(len(o2)):
-        distances.append(numpy.linalg.norm(o1[i] - o2[j]))
-        idxs.append((i, j))
-
-i, j = idxs[numpy.argsort(distances)[0]]
-
-print o1[i], o2[j]
