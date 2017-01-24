2b1c586e6cf853d9b025f30b812c6a5e69f147d6
Ben Bales <bbbales2@gmail.com>
2017-01-23 16:11:12 -0800

    Minor changes to GPC

--- gpc_cubic.py
+++ gpc_cubic.py
@@ -94,48 +94,57 @@ func(2.0, 1.0, 1.0)
 
 #%%
 
+reload(gpc)
+
 func2 = lambda c11, c44 : func(c11, 1.0, c44)
 
-minc11 = 1.6
-maxc11 = 1.8
-minc44 = 0.448
-maxc44 = 0.452
+minc11 = 1.5#1.6
+maxc11 = 2.0#1.8
+minc44 = 0.4#0.448
+maxc44 = 0.6#0.452
 
 hd = gpc.GPC(5, func2, [('u', (minc11, maxc11), 5),
                         ('u', (minc44, maxc44), 5)])
 
 #%%
-def L(c11, c44):
-    logp = 0.0
+for r in range(5, len(data)):
+    def L(c11, c44):
+        logp = 0.0
 
-    u = hd.approx(c11, c44)
-    for i in range(0, len(u)):
-        logp += -0.5 * (data[i] - u[i])**2 / std**2 - numpy.log(std) - 0.5 * numpy.log(2.0) - 0.5 * numpy.log(numpy.pi)
+        u = hd.approx(c11, c44)
+        for i in range(0, r):#len(u)):
+            logp += -0.5 * (data[i] - u[i])**2 / std**2 - numpy.log(std) - 0.5 * numpy.log(2.0) - 0.5 * numpy.log(numpy.pi)
 
-    return numpy.exp(logp)
+        return numpy.exp(logp)
 
-denominator = hd.measure(L)
+    denominator = hd.measure(L)
 
-post = []
-c11s = numpy.linspace(minc11, maxc11, 70)
-c44s = numpy.linspace(minc44, maxc44, 70)
-post = numpy.zeros((len(c11s), len(c44s)))
-for i, z1 in enumerate(c11s):
-    for j, z2 in enumerate(c44s):
-        post[i, j] = L(z1, z2) * hd.prior(z1, z2)
+    minc11 = 1.6
+    maxc11 = 1.8
+    minc44 = 0.445
+    maxc44 = 0.455
 
-post /= sum(sorted(denominator.flatten()))
-        # / denominator#
-#for z in c11s:
-#    post.append(L(z) * hd.prior(z) / denominator)
+    post = []
+    c11s = numpy.linspace(minc11, maxc11, 70)
+    c44s = numpy.linspace(minc44, maxc44, 70)
+    post = numpy.zeros((len(c11s), len(c44s)))
+    for i, z1 in enumerate(c11s):
+        for j, z2 in enumerate(c44s):
+            post[i, j] = L(z1, z2) * hd.prior(z1, z2)
 
-plt.imshow(post, interpolation = 'NONE', extent = [minc44, maxc44, maxc11, minc11], aspect = (maxc44 - minc44) / (maxc11 - minc11))
-plt.colorbar()
-plt.xlabel('c44')
-plt.ylabel('c11')
-#plt.plot(c11s, post)
-#plt.xlabel('c11')
-plt.title('Posterior')
-plt.gcf().set_size_inches((12, 8))
-plt.show()
+    post /= sum(sorted(denominator.flatten()))
+            # / denominator#
+    #for z in c11s:
+    #    post.append(L(z) * hd.prior(z) / denominator)
+
+    plt.imshow(post, interpolation = 'NONE', extent = [minc44, maxc44, maxc11, minc11], aspect = (maxc44 - minc44) / (maxc11 - minc11))
+    #plt.colorbar()
+    plt.xlabel('c44', fontsize = 36)
+    plt.ylabel('c11', fontsize = 36)
+    #plt.plot(c11s, post)
+    #plt.xlabel('c11')
+    plt.title('Posterior, {0} datapoints'.format(r), fontsize = 36)
+    plt.gcf().set_size_inches((12, 8))
+    plt.savefig("/home/bbales2/Documents/group_meeting/jan12/data/{0}.png".format(r), bbox_inches = 'tight', pad_inches = 0.0)
+    plt.show()
 #%%
