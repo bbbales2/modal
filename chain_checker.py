e943a1ca3592c5872a853697530a6f7600c7541c
Ben Bales <bbbales2@gmail.com>
2017-04-18 11:08:17 -0700

    Added some code to verify convergence/mixing of chains

---
+++ chain_checker.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import sys
+import numpy
+import matplotlib.pyplot as plt
+import argparse
+import re
+
+parser = argparse.ArgumentParser(prog='Chain Checker')
+parser.add_argument('--prefix', default = "", nargs='?', help='Discard all lines in files except those with this prefix')
+parser.add_argument('files', nargs='+', help='Files with samples in them')
+
+args = parser.parse_args()
+
+def parse(fname):
+    samples = {}
+    
+    with open(fname) as f:
+        for line in f:
+            if len(args.prefix) > 0 and not re.match(args.prefix, line):
+                continue
+            
+            tokens = line.strip().split(',')
+
+            for key, val in zip(tokens[0::2], tokens[1::2]):
+                key = key.strip()
+                val = float(val)
+                
+                if key not in samples:
+                    samples[key] = []
+
+                samples[key].append(val)
+
+    for key in samples:
+        samples[key] = numpy.array(samples[key])
+
+    return samples
+
+chains = []
+for fname in args.files:
+    chains.append(parse(fname))
+
+ckeys = [set(chain.keys()) for chain in chains]
+
+keys = set.intersection(*ckeys)
+
+if ckeys[0] != keys:
+    raise Exception("Files appear to have differently labeled parameters, or different ones altogether")
+
+if len(ckeys[0]) == 0:
+    raise Exception("At least one file appears to have no keys")
+
+def check_mixing(chains):
+    minl = min(len(chain) for chain in chains)
+
+    chainsPostWarmup = [chain[-minl // 2:] for chain in chains]
+
+    n = (minl // 2) // 2
+
+    chains = []
+    for chain in chainsPostWarmup:
+        chains.append(chain[:n])
+        chains.append(chain[-n:])
+
+    m = len(chains)
+        
+    chainMeans = numpy.mean(chains, axis = 1)
+
+    meanTotal = numpy.mean(chainMeans)
+
+    B = n * sum((chainMeans - meanTotal)**2) / (m - 1)
+
+    chainVars = []
+    for chain, chainMean in zip(chains, chainMeans):
+        chainVars.append(sum((chain - chainMean)**2) / (len(chain) - 1))
+
+    W = sum(chainVars) / m
+
+    var = (n - 1) * W / n + B / n
+
+    return numpy.sqrt(var / W), W, B
+
+for key in sorted(list(keys)):
+    tchains = [chain[key] for chain in chains]
+
+    R, W, B = check_mixing(tchains)
+
+    print "Parameter {0:<5} Rhat {1:<5} Within variance {2:<5} Between variance {3:<5}".format(key, R, W, B)
+    
